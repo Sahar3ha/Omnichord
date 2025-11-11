6bd49c7f15962f7399fae22775ae580b24a12d22
sahar3ha <saharsha.xtha.3@gmail.com>
2025-11-11 15:42:19 +0545

    Appbar added + completed

--- lib/core/common/player_bar.dart
+++ lib/core/common/player_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
 class PlayerBar extends StatefulWidget {
-  
   final AudioPlayer player;
   const PlayerBar({required this.player, super.key});
 
@@ -14,11 +13,11 @@ class _PlayerBarState extends State<PlayerBar> {
   bool isplaying = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    widget.player.playerStateStream.listen((state){
+    widget.player.playerStateStream.listen((state) {
       final playing = state.playing;
-      if(mounted)setState(()=>isplaying = playing );
+      if (mounted) setState(() => isplaying = playing);
     });
   }
 
@@ -30,21 +29,31 @@ class _PlayerBarState extends State<PlayerBar> {
       decoration: BoxDecoration(color: Colors.grey.shade900),
       child: Row(
         children: [
-          const CircleAvatar(radius: 24,backgroundColor: Colors.black26,
-          child: Icon(Icons.music_note),
+          const CircleAvatar(
+            radius: 24,
+            backgroundColor: Colors.black26,
+            child: Icon(Icons.music_note),
           ),
           const SizedBox(width: 12),
-          Expanded(child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const[
-              Text("Playing",
-              style: TextStyle(
-                fontWeight: FontWeight.w600
-              ),
-              )
-            ],
-            ))
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text("Playing", style: TextStyle(fontWeight: FontWeight.w600)),
+                Text('Artist Name', style: TextStyle(fontSize: 12)),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: Icon(isplaying? Icons.pause_circle_filled:Icons.play_circle_fill,size: 36,),
+            onPressed: (){
+              if(isplaying){
+                widget.player.pause();
+              }else{
+                widget.player.play();
+              }
+            }, )
         ],
       ),
     );
